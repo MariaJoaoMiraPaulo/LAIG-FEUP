348dd2237b314950a66456a7960594813b8edb9d
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-10-03 15:44:29 +0100

    Fixing Errors on Class Lights

--- Project 1/reader/Lights.js
+++ Project 1/reader/Lights.js
@@ -1,21 +1,28 @@
 class Lights {
-  constructor(reader, elem) {
-    this.reader = reader;
-    this.elem = elem;
-  }
+    constructor(reader, elem) {
+        this.reader = reader;
+        this.elem = elem;
+    }
+    fillValues() {
+        console.log("ola");
+        console.log(this.reader.getFloat(this.elem.getElementsByTagName('location')[0], 'x'));
+    }
+
 }
 
 class Omni extends Lights {
-  constructor(reader, elem) {
-    super(reader, elem);
-    console.log("omni constructor");
-    console.log(this.reader.getFloat(elem.getElementsByTagName('location')[0],'x'));
-  }
+    constructor(reader, elem) {
+        super(reader, elem);
+        console.log("omni constructor");
+        super.fillValues();
+
+    }
 }
 
-class Spot extends Lights{
-  constructor(reader, elem) {
-    super(reader, elem);
-    console.log("spot constructor");
-  }
+class Spot extends Lights {
+    constructor(reader, elem) {
+        super(reader, elem);
+        console.log("spot constructor");
+        super.fillValues();
+    }
 }
