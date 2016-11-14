1c13ef7ed015ac78946c64d1a6dd4a7c1ef8a5bf
NunoRamos <nunor07.unfc@gmail.com>
2016-11-14 15:50:57 +0000

    Fixing a error

--- Project 2/reader/Animations/LinearAnimation.js
+++ Project 2/reader/Animations/LinearAnimation.js
@@ -1,14 +1,47 @@
 /**
-* LinearAnimation
-* @constructor
-*/
-class LinearAnimation extends Animation{
-  constructor(scene, id, animationTime, controlPoints){
-    super(scene, id, animationTime);
-    this.controlPoints = controlPoints;
-  }
+ * LinearAnimation
+ * @constructor
+ */
+class LinearAnimation extends Animation {
+    constructor(scene, id, animationTime, controlPoints) {
+        super(scene, id, animationTime);
+        this.controlPoints = controlPoints;
 
-  update(deltaTime){
-    console.log("entrei");
-  }
+      //  vec3.subtract(this.direction, this.controlPoints[1], this.controlPoints[0]);
+        this.distanceToNextPoint = 0;
+        this.atualPointIdArray = 1;
+        this.distanceCovered = 0;
+
+        this.totalDist;
+        this.velocity;
+    }
+
+    getTotalDistanceAndVelocity() {
+        this.totalDist = 0;
+
+        for (let i = 0; i < controlPoints.length; i++) {
+            this.totalDist += vec3.length(controlPoints[i]);
+        }
+
+        this.velocity = this.totalDist / this.animationTime;
+    }
+
+    update(deltaTime){
+    /*  let distance =  Math.pow(deltaTime,-3) * this.velocity;
+      this.distanceCovered+=distance;
+      let r = this.distanceCovered /vec3.length(this.direction);
+
+      if(this.distanceCovered+distance > this.distanceToNextPoint){
+        this.distanceCovered+=distance;
+        this.distanceToNextPoint = vec3.length();
+        this.distanceCovered=this.distanceCovered-this.distanceToNextPoint;
+        this.atualPointIdArray++;
+        if(this.atualPointIdArray==this.controlPoints.length){
+          this.atualPointIdArray=0;
+        }
+        else {
+
+        }
+      }*/
+    }
 }
