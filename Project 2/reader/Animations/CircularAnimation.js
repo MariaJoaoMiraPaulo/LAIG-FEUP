978ba2d6d15afee0de52e872142b30bae016a94f
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-11-14 21:42:52 +0000

    Adding circular animtions

--- Project 2/reader/Animations/CircularAnimation.js
+++ Project 2/reader/Animations/CircularAnimation.js
@@ -6,24 +6,16 @@ class CircularAnimation extends Animation{
   constructor(scene, id, animationTime, center, radius, startAng, rotAng){
     super(scene, id, animationTime);
     this.center=center;
-    console.log("Center: "+this.center);
     this.centerX = this.center[0];
     this.centerY = this.center[1];
     this.centerZ = this.center[2];
-    console.log("Center X: "+this.centerX);
-    console.log("Center Y: "+this.centerY);
-    console.log("Center Z: "+this.centerZ);
 
     this.radius = radius;
-    console.log("Raio: "+this.radius);
     this.startAng = startAng;
-    console.log("Angulo Inicial: " + this.startAng);
     this.rotAng = rotAng;
 
-    console.log("Angulo Rotaçao: " + this.rotAng);
-
     this.dist = ((this.rotAng*Math.PI)/180)*this.radius;
-    this.atualdist;
+    this.atualdist=0;
     this.atualAngle=0;
     console.log("Dist:"+this.dist);
 
@@ -38,13 +30,12 @@ class CircularAnimation extends Animation{
   }
 
   update(deltaTime){
-    if(this.atualAngle<this.rotAng){
-      this.atualdist = Math.pow(deltaTime, -3)*this.dist;
-      this.atualAngle+= this.atualdist/this.radius;
+    if(this.atualAngle<((this.rotAng*Math.PI)/180)){
+      this.atualdist+= ((deltaTime/1000)*this.dist)/(this.animationTime/1000);
+      this.atualAngle = this.atualdist/this.radius;
       console.log("atual \n" + this.atualAngle);
-      console.log("rot \n" + this.rotAng);
     }
-    console.log("acabei");
+    else console.log("acabei");
   }
 
     clone() {
