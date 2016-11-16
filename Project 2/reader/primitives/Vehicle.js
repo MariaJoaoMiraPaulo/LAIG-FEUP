4a5ffffaff2188885478855bb49042cf365b66ae
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-11-16 19:14:11 +0000

    Continuing Vehicle

--- Project 2/reader/primitives/Vehicle.js
+++ Project 2/reader/primitives/Vehicle.js
@@ -2,14 +2,24 @@ function Vehicle(scene, reader) {
 	CGFobject.call(this,scene);
 	this.scene = scene;
 	this.reader = reader;
-	var points = [[-1.5,0,1,1],[-2,1,2.4,1],[-2,3,-3.5,1],[-1.5,6,-5,1],
-								[0,0,1,1],[0,1,2.4,1],[0,3,-3.5,1],[0,6,-5,1],
-								[1,0,1,1],[1,1,2.4,1],[1,3,-3.5,1],[1,6,-5,1],
-								[1.5,0,1,1],[2,1,2.4,1],[2,3,-3.5,1],[1.5,6,-5,1]];
+	var points = [[-1.5,0,1,1],[-2,1,0.8,1],[-2,1,-0.8,1],[-1.5,2,-0.8,1],
+								[0,0,1,1],[0,1,0.8,1],[0,1,-0.8,1],[0,2,-0.8,1],
+								[1,0,1,1],[1,1,0.8,1],[1,1,-0.8,1],[1,2,-0.8,1],
+								[1.5,0,1,1],[2,1,0.8,1],[2,1,-0.8,1],[1.5,2,-0.8,1]];
 
-	console.log("entrei");
-	console.log(points);
+var points2 = [[1.5,0,1,1],[2,1,0.8,1],[2,1,-0.8,1],[1.5,2,-0.8,1],
+								[1.5,0,-1,1],[1.5,0.8,-1,1],[1.5,1,-1,1],[1.5,2,-1,1],
+								[1.5,0,-1,1],[1.5,0.8,-1,1],[1.5,1,-1,1],[1.5,2,-1,1]];
+
+var points3 = [[-1.5,0,1,1],[-2,1,0.8,1],[-2,1,-0.8,1],[-1.5,2,-0.8,1],
+								[-1.5,0,-1,1],[-1.5,0.8,-1,1],[-1.5,1,-1,1],[-1.5,2,-1,1],
+								[-1.5,0,-1,1],[-1.5,0.8,-1,1],[-1.5,1,-1,1],[-1.5,2,-1,1]];
+
+
+
 	this.front = new Patch(this.scene,3,3,20,20,points);
+	this.doorLeft = new Patch(this.scene,2,3,20,20,points2);
+	this.doorRight = new Patch(this.scene,2,3,20,20,points3);
 
 };
 
@@ -23,6 +33,15 @@ Vehicle.prototype.display = function() {
 
 this.scene.pushMatrix();
 	this.front.display();
+this.scene.popMatrix();
+
+this.scene.pushMatrix();
+	this.doorLeft.display();
+this.scene.popMatrix();
+
+this.scene.pushMatrix();
+	//this.scene.rotate(Math.PI,0,1,0);
+//	this.doorRight.display();
 this.scene.popMatrix();
 }
 
