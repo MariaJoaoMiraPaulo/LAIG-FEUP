a550906980ffdd97056e054d16fd269757a8b46f
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-12-22 19:05:34 +0000

    Fixing details on Class Player. Moving Pawn to Start Positions

--- Project 3/reader/game/Player.js
+++ Project 3/reader/game/Player.js
@@ -5,14 +5,19 @@ class Player {
       this.score = 0;
       this.walls = 9;
 
+
       switch (player) {
         case 1:
             this.pawn1 = "p11";  //id do componente com a peça 1 do player 1
             this.pawn2 = "p12";  //id do componente com a peça 2 do player 1
+            this.startPositionPawn1=this.graph.components["board"].childrens[0].startPos11;
+            this.startPositionPawn2=this.graph.components["board"].childrens[0].startPos12;
             break;
         case 2:
             this.pawn1 = "p21"; //id do componente com a peça 1 do player 2
             this.pawn2 = "p22"; //id do componente com a peça 2 do player 2
+            this.startPositionPawn1=this.graph.components["board"].childrens[0].startPos21;
+            this.startPositionPawn2=this.graph.components["board"].childrens[0].startPos22;
             break;
         default:
 
@@ -44,13 +49,8 @@ class Player {
     }
 
     movePawnToStartPosition(){
-      if(this.player==1){
-        this.graph.components[this.pawn1].transformationMatrix=this.graph.getTransformationMatrix([["translate",3,0.5,3]]);
-        this.graph.components[this.pawn2].transformationMatrix=this.graph.getTransformationMatrix([["translate",8,0.5,3]]);
+        this.graph.components[this.pawn1].transformationMatrix=this.graph.getTransformationMatrix([["translate",this.startPositionPawn1[0],this.startPositionPawn1[1],this.startPositionPawn1[2]]]);
+        this.graph.components[this.pawn2].transformationMatrix=this.graph.getTransformationMatrix([["translate",this.startPositionPawn2[0],this.startPositionPawn2[1],this.startPositionPawn2[2]]]);
       }
-      else if(this.player==2) {
-        this.graph.components[this.pawn1].transformationMatrix=this.graph.getTransformationMatrix([["translate",3,0.5,4]]);;
-        this.graph.components[this.pawn2].transformationMatrix=this.graph.getTransformationMatrix([["translate",8,0.5,4]]);;
-      }
-    }
+
 }
